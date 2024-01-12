cfc7074ef5c112b5b88f0a1eea335c94493ef5ca
엄채연 EOM CHAE YEON <43158518+sorimchuku@users.noreply.github.com>
2024-01-12 13:32:48 +0900

    파이어베이스 연동
    데이터 읽기, 쓰기, 갱신 기능 간단하게 구현

--- hey_just_do/lib/main.dart
+++ hey_just_do/lib/main.dart
@@ -1,9 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:hey_just_do/firebase_options.dart';
 
-void main() {
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MainApp());
 }
-
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
@@ -11,10 +18,118 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'heyjustdo',
-      home: Scaffold(
-        body: Center(
-          child: Text('그냥해봅시다!!'),
+      home: const MainScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({Key? key}) : super(key: key);
+
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  String? currentId;
+  String? mission;
+  int? entryCount;
+
+  @override
+  void initState() {
+    super.initState();
+    readData();
+  }
+
+  void readData() {
+    final missionsCollectionReference = FirebaseFirestore.instance.collection("missions");
+
+    missionsCollectionReference
+        .orderBy(FieldPath.documentId)
+        .limit(1)
+        .get()
+        .then((querySnapshot) {
+      if (querySnapshot.docs.isNotEmpty) {
+        setState(() {
+          mission = querySnapshot.docs.first.data()?['mission'];
+          entryCount = querySnapshot.docs.first.data()?['entryCount'];
+          currentId = querySnapshot.docs.first.id;
+        });
+      } else {
+        print('No data available');
+      }
+    });
+  }
+
+  void getNextData() {
+    if (currentId != null) {
+      final missionsCollectionReference = FirebaseFirestore.instance.collection("missions");
+
+      missionsCollectionReference
+          .where(FieldPath.documentId, isGreaterThan: currentId!)
+          .orderBy(FieldPath.documentId)
+          .limit(1)
+          .get()
+          .then((querySnapshot) {
+        if (querySnapshot.docs.isNotEmpty) {
+          setState(() {
+            mission = querySnapshot.docs.first.data()?['mission'];
+            entryCount = querySnapshot.docs.first.data()?['entryCount'];
+            currentId = querySnapshot.docs.first.id;
+          });
+        } else {
+          print('No more data available');
+        }
+      });
+    }
+  }
+
+  void participate() {
+    int updatedEntryCount = (entryCount ?? 0) + 1;
+    final missionsCollectionReference = FirebaseFirestore.instance.collection("missions");
+
+    missionsCollectionReference.doc(currentId).update({
+      'entryCount': updatedEntryCount,
+    }).then((value) {
+      setState(() {
+        entryCount = updatedEntryCount;
+      });
+    }).catchError((error) {
+      print("Failed to update entryCount: $error");
+    });
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "그냥해!",
+              style: TextStyle(fontSize: 20),
+            ),
+            SizedBox(height: 20),
+            if (mission != null && entryCount != null)
+              Text(
+                "오늘의 미션: $mission",
+                style: TextStyle(fontSize: 28),
+              ),
+            Text(
+              "참여자수: $entryCount",
+              style: TextStyle(fontSize: 16),
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [ElevatedButton(onPressed: getNextData, child: Text('갱신'),),
+                SizedBox(width: 16,),
+                ElevatedButton(onPressed: participate, child: Text('미션 시작'),),],
+            )
+
+          ],
         ),
       ),
     );
